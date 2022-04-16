da0bc420c7572d99ff21802661ba6db8dcfb2698
Kiera Carman <kiera.carman@gmail.com>
2022-04-16 19:23:02 -0400

    Fix another bug

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,10 +1,8 @@
 import { NextResponse } from 'next/server'
 
-export function middleware(req, ev) {
-  const currentEnv = process.env.NODE_ENV
-
-  if (currentEnv === 'production' &&
-      req.headers.get("x-forwarded-proto") !== "https") {
+export function middleware(req) {
+  if (process.env.NODE_ENV === 'production' &&
+      req.headers.get('x-forwarded-proto') !== 'https') {
     return NextResponse.redirect(
       `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
       301
