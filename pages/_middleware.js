5c6bca79275d05bd08d3f47f897ef3fb7d076dc3
Kiera Carman <kiera.carman@gmail.com>
2022-04-16 19:10:04 -0400

    Add middleware to redirect force https

---
+++ pages/_middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req, ev) {
+  const currentEnv = process.env.NODE_ENV
+
+  if (currentEnv === 'production' &&
+      req.headers.get("x-forwarded-proto") !== "https") {
+    return NextResponse.redirect(
+      `https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
+      301
+    )
+  }
+  return NextResponse.next()
+}
