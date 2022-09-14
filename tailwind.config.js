0ae80a9f932b83e94a8fd440e5860e0fcdac93cb
Kiera Carman <kiera.carman@gmail.com>
2022-09-14 00:42:19 -0400

    Add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      background: '#021634',
+      'background-light': '#1c3254',
+      white: '#ffffff',
+      blush: '#ffeaee',
+      accent: '#75e3f0',
+      cta: '#f2a6c6'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
