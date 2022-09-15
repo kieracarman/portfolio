bd140254e4c3538a4e60113bc614fbb2a1d66d01
Kiera Carman <kiera.carman@gmail.com>
2022-09-15 13:58:48 -0400

    Reorder tailwind classes with prettier

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const About = () => {
   return (
     <section className='prose-lg mb-12 px-4'>
-      <h2 className='text-accent mt-6 mb-4 font-extrabold'>
+      <h2 className='mt-6 mb-4 font-extrabold text-accent'>
         Hi, my name is Kiera!
       </h2>
       <p>
@@ -22,14 +22,14 @@ const About = () => {
         Outside of coding, I love building things with my hands, cooking at
         home, and DJing killer parties when we&lsquo;re not in a pandemic.
       </p> */}
-      <div className='flex justify-center gap-4 mt-8'>
+      <div className='mt-8 flex justify-center gap-4'>
         <Link href='https://github.com/kieracarman'>
-          <a className='block px-8 py-2 text-center font-bold transition duration-200 border rounded-lg focus:outline-none text-background bg-cta  border-cta hover:text-white hover:border-white'>
+          <a className='block rounded-lg border border-cta bg-cta px-8 py-2 text-center font-bold text-background transition duration-200  hover:border-white hover:text-white focus:outline-none'>
             Github
           </a>
         </Link>
         <Link href='https://linkedin.com/in/kiera-carman'>
-          <a className='block px-8 py-2 text-center transition duration-200 border rounded-lg focus:outline-none text-cta border-cta hover:border-white hover:text-white'>
+          <a className='block rounded-lg border border-cta px-8 py-2 text-center text-cta transition duration-200 hover:border-white hover:text-white focus:outline-none'>
             Linkedin
           </a>
         </Link>
