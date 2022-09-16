e4115b58843c6051792fcd20dc752b34f5525844
Kiera Carman <kiera.carman@gmail.com>
2022-09-15 21:23:34 -0400

    Fix border

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react'
 
 const Browser = ({ children }: PropsWithChildren) => {
   return (
-    <div className='w-full rounded-xl outline outline-1 outline-background-light'>
-      <div className='flex items-center justify-between rounded-t-xl bg-background py-2 px-2'>
+    <div className='w-full overflow-hidden rounded-xl border border-background-light'>
+      <div className='flex items-center justify-between bg-background py-2 px-2'>
         <div className='ml-1 flex w-14 justify-between'>
           <div className='h-3 w-3 rounded-full bg-alert'></div>
           <div className='h-3 w-3 rounded-full bg-warn'></div>
@@ -11,7 +11,7 @@ const Browser = ({ children }: PropsWithChildren) => {
         </div>
         <div className='ml-3 h-5 w-full rounded-md bg-background-light'></div>
       </div>
-      <div className='overflow-hidden rounded-b-xl'>{children}</div>
+      <div>{children}</div>
     </div>
   )
 }
