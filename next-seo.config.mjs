0441d10fbf7ad9127ab8e918bf2d11cda5031c27
Kiera Carman <kiera.carman@gmail.com>
2022-10-06 14:30:36 -0400

    Add OpenGraph tags

--- next-seo.config.mjs
+++ next-seo.config.mjs
@@ -1,3 +1,10 @@
+// eslint-disable-next-line
 export default {
-  titleTemplate: 'Kiera Carman | %s'
+  titleTemplate: 'Kiera Carman | %s',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.kieracarman.com',
+    title: 'Kiera Carman | %s',
+    description: 'Personal portfolio. Check out my projects or get in touch!'
+  }
 }
